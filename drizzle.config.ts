8adfcbb392e0d799f4919dcd049b461dc4fd5f23
François Dabiel <dabielf@gmail.com>
2025-02-10 09:23:12 -0600

    added d1 migrations possibility

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+	schema: "./src/lib/server/db/schema.ts",
+	out: "./src/lib/server/db/migrations",
+	driver: "d1-http",
+
+	dbCredentials: {
+		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+		token: process.env.CLOUDFLARE_D1_TOKEN!,
+	},
+
+	verbose: true,
+	strict: true,
+	dialect: "sqlite",
+});
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import { Hono } from "hono";
 import userRouter from "./routes/users";
+import { bearerAuth } from "hono/bearer-auth";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.get("/", (c) => {
+app.get("/api", (c) => {
 	return c.text("Hello François, welcome to your API!");
 });
 
 // Mount user routes under /users
-app.route("/users", userRouter);
+app.route("/api/users", userRouter);
+
+// app.use(
+// 	"/auth-verify-token/*",
+// 	bearerAuth({
+// 		verifyToken: async (token, c) => {
+// 			return token === "dynamic-token";
+// 		},
+// 	}),
+// );
 
 export default app;
